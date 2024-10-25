c938f19193ac41f0829e172fd28f6672103e7197
LPT-30 <LPT-30@10.0.0.104>
2024-10-25 14:47:14 +0530

    commit2

---
+++ sri_svkp_files/src/Basicsofjava/VariablesDemo.java
@@ -0,0 +1,18 @@
+package Basicsofjava;
+public class VariablesDemo {
+			int id=10; //instance variable
+			void print()
+			{
+				String msg="Hii"; //local variable
+				System.out.println(msg);
+			}
+	static String message="I am Drakshayani"; //static variable
+	public static void main(String[] args) {
+		VariablesDemo obj=new VariablesDemo();
+		System.out.println(obj.id);
+		obj.print();
+		System.out.println(message);
+		
+	}
+
+}
