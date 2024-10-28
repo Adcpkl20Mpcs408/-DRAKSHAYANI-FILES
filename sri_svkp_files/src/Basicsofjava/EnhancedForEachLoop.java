25ff68e36777a14d6f510fe09119c693c62250e3
LPT-30 <LPT-30@DESKTOP-EBBSBVQ>
2024-10-28 17:36:52 +0530

    commit13

---
+++ sri_svkp_files/src/Basicsofjava/EnhancedForEachLoop.java
@@ -0,0 +1,15 @@
+package Basicsofjava;
+
+public class EnhancedForEachLoop {
+
+	public static void main(String[] args) {
+		int num[]= {1,3,5,6,4,4,6,9};
+		for(int numbers :num)
+		{
+			System.out.println(numbers);
+			
+		}
+
+	}
+
+}
