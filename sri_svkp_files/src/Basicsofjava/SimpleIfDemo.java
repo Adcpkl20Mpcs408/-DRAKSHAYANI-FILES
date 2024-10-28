4b37853cdd49c2f23e6423cc37fd28c239d9bff8
LPT-30 <LPT-30@DESKTOP-EBBSBVQ>
2024-10-28 16:25:46 +0530

    commit7

---
+++ sri_svkp_files/src/Basicsofjava/SimpleIfDemo.java
@@ -0,0 +1,16 @@
+package Basicsofjava;
+public class SimpleIfDemo {
+
+	public static void main(String[] args) {
+		int age=19;
+		if(age>18)
+		{
+			System.out.println("you are eligible for vote");
+		}
+		else
+		{
+			System.out.println("you are not eligible for vote");
+		}
+	}
+
+}
