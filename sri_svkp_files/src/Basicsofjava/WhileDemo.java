edb34b1b158299a657984b6a24449ef7cc6aaac0
LPT-30 <LPT-30@DESKTOP-EBBSBVQ>
2024-10-28 17:12:53 +0530

    commit11

---
+++ sri_svkp_files/src/Basicsofjava/WhileDemo.java
@@ -0,0 +1,14 @@
+package Basicsofjava;
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		int num=5,i=1;
+		while(i<=10) //1<10
+		{
+			System.out.printf("%d * %d = %d\n",num,i,num*i);
+			i++;
+		}
+
+	}
+
+}
