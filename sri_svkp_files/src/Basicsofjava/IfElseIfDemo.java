c3f1a98325a95304daa30e8a6b16198783276955
LPT-30 <LPT-30@DESKTOP-EBBSBVQ>
2024-10-28 16:36:25 +0530

    commit8

---
+++ sri_svkp_files/src/Basicsofjava/IfElseIfDemo.java
@@ -0,0 +1,42 @@
+package Basicsofjava;
+public class IfElseIfDemo {
+	public static void main(String[] args) {
+		
+		int score=92;
+		
+		if(score>90)
+		{
+			System.out.println("Grade O");
+		}
+		else if(score>=80)
+		{
+			System.out.println("Grade A+");
+		}
+		else if(score>=70)
+		{
+			System.out.println("Grade A");
+		}
+		else if(score>60)
+		{
+			System.out.println("Grade B+");
+		}
+		else if(score>50)
+		{
+			System.out.println("Grade B+");
+		}
+		else if(score>40)
+		{
+			System.out.println("Grade C");
+		}
+		else if(score>30)
+		{
+			System.out.println("Grade D");
+		}
+		else
+		{
+			System.out.println("Fail");
+		}
+
+	}
+
+}
