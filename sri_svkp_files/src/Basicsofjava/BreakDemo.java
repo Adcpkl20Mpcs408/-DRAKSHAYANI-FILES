683e3dd78b4b3f729ce03dd65bbf5538cd3de809
LPT-30 <LPT-30@10.0.0.112>
2024-11-01 15:50:19 +0530

    commit13

---
+++ sri_svkp_files/src/Basicsofjava/BreakDemo.java
@@ -0,0 +1,16 @@
+package Basicsofjava;
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		for(int i=1;i<50;i++)
+		{
+			if(i>40)
+			{
+				break;
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
