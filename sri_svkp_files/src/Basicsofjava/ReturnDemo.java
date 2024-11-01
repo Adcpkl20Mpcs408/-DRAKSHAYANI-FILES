a6647a31a4120d5bb7d15597cf291a0910867918
LPT-30 <LPT-30@10.0.0.112>
2024-11-01 16:04:01 +0530

    commit15

---
+++ sri_svkp_files/src/Basicsofjava/ReturnDemo.java
@@ -0,0 +1,13 @@
+package Basicsofjava;
+
+public class ReturnDemo {
+
+	public static void main(String[] args) {
+		System.out.println(sum(15,10,5));
+		}
+	static int sum(int a,int b,int c)
+	{
+		return a+b+c;
+	}
+
+}
