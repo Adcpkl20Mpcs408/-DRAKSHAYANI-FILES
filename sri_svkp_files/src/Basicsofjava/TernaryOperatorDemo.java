60ae42d72082bd1b2e556eceb6e5f0400b41e064
LPT-30 <LPT-30@10.0.0.111>
2024-11-04 16:08:27 +0530

    commit21

---
+++ sri_svkp_files/src/Basicsofjava/TernaryOperatorDemo.java
@@ -0,0 +1,11 @@
+package Basicsofjava;
+
+public class TernaryOperatorDemo {
+	public static void main(String[] args) {
+		int a=10,b=50;
+		int max=(a>b)?a:b;
+		System.out.println("max value is : " + max);
+
+	}
+
+}
