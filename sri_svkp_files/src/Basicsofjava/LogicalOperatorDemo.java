e8588f59d78152262a53748d42bfac4b223dd999
LPT-30 <LPT-30@10.0.0.111>
2024-11-04 14:31:36 +0530

    commit17

---
+++ sri_svkp_files/src/Basicsofjava/LogicalOperatorDemo.java
@@ -0,0 +1,22 @@
+package Basicsofjava;
+
+public class LogicalOperatorDemo {
+
+	public static void main(String[] args) {
+		int a=20 ,b=10,c=5;
+		
+		//Logical AND
+		
+		System.out.println("(a>b) && (b<c)" + ((a>b) && (b<c))); //false
+		
+		//Logical OR
+		
+		System.out.println("(a>b) || (b>c)" + ((a>b) || (b>c) ));//true
+		
+		//Logical NOT
+		
+		System.out.println("!(a>b) " + !(a>b));//false
+
+	}
+
+}
