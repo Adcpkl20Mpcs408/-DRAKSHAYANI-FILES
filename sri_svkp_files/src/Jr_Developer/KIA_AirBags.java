3ea50e63ae39d3c213b06a3859eb1c51709fb125
LPT-30 <LPT-30@10.0.0.106>
2024-11-19 14:33:59 +0530

    Commit3

---
+++ sri_svkp_files/src/Jr_Developer/KIA_AirBags.java
@@ -0,0 +1,12 @@
+package Jr_Developer;
+
+public class KIA_AirBags {
+	
+	public void disp_AirBags()
+
+	{
+		System.out.println("All KIA have airbags ");
+	}
+	 
+	
+}
---
+++ sri_svkp_files/src/Jr_Developer/KIA_Speed_limit.java
@@ -0,0 +1,9 @@
+package Jr_Developer;
+
+public class KIA_Speed_limit {
+
+	 public void disp_Speed_limit()
+	{
+		System.out.println("All KIA have speedlimit ");
+	}
+}
