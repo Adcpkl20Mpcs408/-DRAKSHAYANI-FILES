82538f708fe5ceecf21ae1b55a4c30d96ae2f97a
LPT-30 <LPT-30@10.0.0.107>
2024-12-13 13:53:47 +0530

    seventh commit

---
+++ sri_svkp_files/src/org/tnsif/capgemini/c2tc/Jr_Developer/KIA_AirBags.java
@@ -0,0 +1,12 @@
+package org.tnsif.capgemini.c2tc.Jr_Developer;
+
+public class KIA_AirBags {
+	
+	public void disp_AirBags()
+
+	{
+		System.out.println("All KIA have airbags ");
+	}
+	 
+	
+}
---
+++ sri_svkp_files/src/org/tnsif/capgemini/c2tc/Jr_Developer/KIA_Speed_limit.java
@@ -0,0 +1,9 @@
+package org.tnsif.capgemini.c2tc.Jr_Developer;
+
+public class KIA_Speed_limit {
+
+	 public void disp_Speed_limit()
+	{
+		System.out.println("All KIA have speedlimit ");
+	}
+}
